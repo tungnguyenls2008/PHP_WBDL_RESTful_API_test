2f9cdcb81b68dac3d91cc56091ce8b4f0ee3f56b
OverLord <overlord@localhost.localdomain>
2020-06-02 11:38:29 +0700

    homewrk

---
+++ app/Repositories/Impl/CustomerRepositoryImpl.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Repositories\Impl;
+
+
+use App\Customer;
+use App\Repositories\CustomerRepository;
+use App\Repositories\Eloquent\EloquentRepository;
+
+class CustomerRepositoryImpl extends EloquentRepository  implements CustomerRepository
+{
+    /**
+     * get Model
+     * @return string
+     */
+    public function getModel()
+    {
+        $model = Customer::class;
+        return $model;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/*Route::get('/customers', 'CustomerController@index')->name('customers.all');
+Route::get('/customers/{customerId}', 'CustomerController@show')->name('customers.show');
+Route::post('/customers', 'CustomerController@store')->name('customers.store');
+Route::put('/customers/{customerId}', 'CustomerController@update')->name('customers.update');
+Route::delete('/customers/{customerId}', 'CustomerController@destroy')->name('customers.destroy');*/
